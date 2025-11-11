00f372e03355feca56fa9eb5af0cb2ec50b6e033
ompatil2024it-jpg <ompatil2024.it@mmcoe.edu.in>
2025-11-11 12:28:26 +0530

    Add files via upload

---
+++ B-14/src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+
+import { MainLayout } from '@/components/main-layout';
+import { Profile } from '@/components/profile';
+
+export default function ProfilePage() {
+  return (
+    <MainLayout>
+      <Profile />
+    </MainLayout>
+  );
+}
