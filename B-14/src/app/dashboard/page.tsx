d48a6614d5b1d2f8fcb79f2f7ff5ba394f1dc584
ompatil2024it-jpg <ompatil2024.it@mmcoe.edu.in>
2025-11-11 12:25:45 +0530

    Add files via upload

---
+++ B-14/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+import { MainLayout } from '@/components/main-layout';
+import { Dashboard } from '@/components/dashboard';
+
+export default function DashboardPage() {
+  return (
+    <MainLayout>
+      <Dashboard />
+    </MainLayout>
+  );
+}
