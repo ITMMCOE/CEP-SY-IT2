e97691d7eca42fafeb3a1fd8efed4df6265cbb50
mukeshgajrushi2024it <mukeshgajrushi2024.it@mmcoe.edu.in>
2025-11-12 11:04:58 +0530

    Add files via upload

---
+++ B17 /src /firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "studio-5977076642-e0c90",
+  "appId": "1:875898429630:web:7cbd434103c733d1207eef",
+  "apiKey": "AIzaSyBBqDGXRfgh85WF4aLk3nS3-M2_1tBQpZU",
+  "authDomain": "studio-5977076642-e0c90.firebaseapp.com",
+  "storageBucket": "studio-5977076642-e0c90.appspot.com",
+  "measurementId": "",
+  "messagingSenderId": "875898429630"
+};
